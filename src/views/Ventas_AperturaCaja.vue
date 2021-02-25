8ce33e4ae2f929149ac412c149c6ac91a72d494e
vfirpo <victor.firpo@gmail.com>
2021-02-25 17:08:00 -0300

    HTML

--- src/views/Ventas_AperturaCaja.vue
+++ src/views/Ventas_AperturaCaja.vue
@@ -5,10 +5,10 @@
     </div>
     <div class="container " >
       <!--desde Datalive -->
-      <div>
-        <label for="ctrl_fecha">Fecha:</label>
-        <div>
-          <vc-date-picker
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col class="col-2" ><strong><label for="ctrl_fecha">Fecha:</label></strong></b-col>
+          <b-col class="col-3"><vc-date-picker
             v-model="fecha"
             id="ctrl_fecha"
             name="ctrl_fecha"
@@ -20,33 +20,28 @@
                 v-on="inputEvents"
               />
             </template>
-          </vc-date-picker>
-        </div>
-      </div>
-
-      <div>
-        <label for="ctrl_turnos">Turno:</label>
-        <div>
-          <b-form-select
+          </vc-date-picker></b-col>
+        </b-row>
+        <b-row>
+          <b-col><strong><label for="ctrl_turnos">Turno:</label></strong></b-col>
+          <b-col>          <b-form-select
             v-model="turno"
             :options="turnos"
             id="ctrl_turnos"
             name="ctrl_turnos"
-          />
-        </div>
-      </div>
-
-      <div>
-        <label for="ctrl_importe"
-          >Dinero apertura::</label>
-        <div>
-          <b-input
+          /></b-col>
+        </b-row>
+        <b-row>
+          <b-col><strong><label for="ctrl_importe"
+          >Dinero apertura::</label></strong></b-col>
+          <b-col>          <b-input
             type="number"
             name="ctrl_importe"
             id="ctrl_importe"
-          />
-        </div>
-      </div>
+          /></b-col>
+        </b-row>
+      </b-container>
+      <br><br>
       <!-- Desde Datalive-->
       <b-button variant="primary" @click="abrirCaja()">Abrir Caja</b-button>
       <b-button variant="secondary" @click="$router.go(-1)">Volver</b-button>
