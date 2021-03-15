5e9b1403631254f0dc55d1cf13acc4f2e56c82ca
vfirpo <victor.firpo@gmail.com>
2021-03-15 14:51:35 -0300

    cambios

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -7,17 +7,23 @@
         id="spinner"
       ></b-spinner>
     </div>
-    <div class= "d-flex flex-row">
-    <orderMainPanel :filter="filter"/>
-    </div>
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-      <b-buttons type="button" class="btn btn-primary p-4 m-3">
-        tomar pedido mostrador
-      </b-buttons>
-      <button type="button" class="btn btn-secondary p-4 m-3">
-        tomar pedido delivery
-      </button>
-    </div>
+    <div class="container-fluid">
+      <div class= "row align-items-start">
+        <div class="col">
+        <orderMainPanel :filter="filter"/>
+        </div>
+        <div class="col">
+        <div class= "align items-end">
+          <button type="button" class="btn btn-primary p-3 m-3">
+            tomar pedido mostrador
+          </button>
+          <button type="button" class="btn btn-secondary p-3 m-3">
+            tomar pedido delivery
+          </button>
+        </div>
+        </div>
+      </div>
+    </div>  
     <Comandas />
   </div>
 </template>
@@ -37,20 +43,20 @@ export default {
   data() {
     return {
       dato: null,
-      filter:{
-          rendidas: Boolean,
-          anuladas: Boolean,
-          cobradas: Boolean,
-          sinCobrar: Boolean,
-          efectivo: Boolean,
-          electronico:    Boolean   
-      }
+      filter: {
+        rendidas: Boolean,
+        anuladas: Boolean,
+        cobradas: Boolean,
+        sinCobrar: Boolean,
+        efectivo: Boolean,
+        electronico: Boolean,
+      },
     };
   },
   mounted() {
     document.getElementById("header_principal").hidden = true;
 
-    this.setDefaultFilters()
+    this.setDefaultFilters();
 
     this.windowonload();
   },
@@ -66,14 +72,14 @@ export default {
       contenedor.style.visibility("hidden");
       contenedor.style.opacity("0");
     },
-    setDefaultFilters(){
-      this.filter.rendidas = false
-      this.filter.anuladas = false
-      this.filter.cobradas = true
-      this.filter.sinCobrar = true
-      this.filter.efectivo = true
-      this.filter.electronico = true
-    }
+    setDefaultFilters() {
+      this.filter.rendidas = false;
+      this.filter.anuladas = false;
+      this.filter.cobradas = true;
+      this.filter.sinCobrar = true;
+      this.filter.efectivo = true;
+      this.filter.electronico = true;
+    },
   },
 };
 </script>
